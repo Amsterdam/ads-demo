4a25eb0f932b3a1937549380850b2a27bf8a8619
Aram Limpens <a.limpens@amsterdam.nl>
2023-09-07 17:05:55 +0200

    Remove deprecated package

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import '@amsterdam/design-system-tokens/dist/root.css'
-import '@amsterdam/design-system-font/static/index.css'
+import '@amsterdam/design-system-assets/font/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
