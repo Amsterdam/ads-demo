00758659665bc9978de5e28f498e6bb1809b7bb8
Bettina01 <bettina.wieringa@gmail.com>
2023-10-20 13:58:12 +0200

    Nog een kleine wijziging

--- src/pages/testpage/testpage.jsx
+++ src/pages/testpage/testpage.jsx
@@ -77,7 +77,7 @@ const Testpage = ({ grid }) => {
             level={4}
             style={{ marginTop: '56px', marginBottom: '12px' }}
           >
-            Meer feiten en cijfers03
+            Meer feiten en cijfers04
           </Heading>
           <Styled.List>
             {moreLinks.map((item) => (
